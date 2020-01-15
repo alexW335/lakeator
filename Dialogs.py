e5bde021b4afb3554c2a7a0b00b281eac0890951
Alex White <alexwhite335@gmail.com>
2020-01-15 17:23:32 +1300

    Changed settings to be in nested dict structure saved to settings.txt with json.dumps

--- Dialogs.py
+++ Dialogs.py
@@ -1,4 +1,5 @@
 from matplotlib.backends.qt_compat import QtCore, QtWidgets, QtGui
+from numpy import array
 
 class GPSPopUp(QtWidgets.QDialog):
     # Class for the set GPS information dialog box
@@ -50,3 +51,43 @@ class GPSPopUp(QtWidgets.QDialog):
         pEPSG = int(self.name3.text())
         tEPSG = int(self.name4.text())
         return [lat, long, EPSG, pEPSG, tEPSG]
+
+    
+class MicPositionPopUp(QtWidgets.QDialog):
+    # Class for the set GPS information dialog box
+    def __init__(self, cur_locs=array([[0,0],[0,1],[1,0],[1,1]]), parent=None):
+        QtWidgets.QDialog.__init__(self, parent)
+        self.setWindowTitle('Set GPS Array Information')
+        self.setWindowIcon(QtGui.QIcon('kiwi.png'))
+        # self.setWindowFlags((self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint) | QtCore.Qt.WindowStaysOnTopHint)
+        self.setMinimumWidth(400)
+
+        self.coordsLabel = QtWidgets.QLabel("Coordinates")
+        self.name1 = QtWidgets.QPlainTextEdit(self)
+        self.name1.setPlainText(self.arrayToStr(cur_locs))
+
+        self.activate = QtWidgets.QPushButton("Set")
+
+        Box = QtWidgets.QVBoxLayout()
+        Box.addWidget(self.coordsLabel)
+        Box.addWidget(self.name1)
+        Box.addWidget(self.activate)
+
+        # Now put everything into the frame
+        self.setLayout(Box)
+    
+    def arrayToStr(self, arr):
+        strout = ""
+        for row in range(arr.shape[0]):
+            strout += "{}, {}\n".format(arr[row, 0], arr[row, 1])
+        return strout
+    
+    def getValues(self):
+        tmp = []
+        print(r"{}".format(self.name1.toPlainText()))
+        instr = self.name1.toPlainText().strip().split('\n')
+        for ind in instr:
+            a = ind.split(',')
+            tmp.append([float(a[0].strip()), float(a[1].strip())])
+        tmp = array(tmp)
+        return tmp
--- ex.py
+++ ex.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import lakeator
 import Dialogs
+import json
 from copy import copy
 
 # from PyQt5 import QtGui
@@ -14,6 +15,8 @@ from matplotlib.pyplot import colormaps
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self._load_settings()
+
         self._main = QtWidgets.QWidget()
         self.setCentralWidget(self._main)
         layout = QtWidgets.QHBoxLayout(self._main)
@@ -50,7 +53,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         setArrayDesign = QtWidgets.QAction("&Configure Array Design", self)
         setArrayDesign.setShortcut("Ctrl+A")
         setArrayDesign.setStatusTip("Input relative microphone positions and array bearing.")
-        setArrayDesign.triggered.connect(self.file_open)
+        setArrayDesign.triggered.connect(self.get_array_info)
 
         setGPSCoords = QtWidgets.QAction("&Set GPS Coordinates", self)
         setGPSCoords.setShortcut("Ctrl+C")
@@ -70,6 +73,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         changeMethod.setShortcut("Ctrl+M")
         changeMethod.setStatusTip("Switch between and configure the AF-MUSIC and GCC algorithms.")
         changeMethod.triggered.connect(self.file_open)
+        changeMethod.setDisabled(True)
 
         self.refreshHeatmap = QtWidgets.QAction("&Calculate", self)
         self.refreshHeatmap.setShortcut("Ctrl+H")
@@ -81,8 +85,6 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         heatmapMenu.addAction(setDomain)
         heatmapMenu.addAction(changeMethod)
 
-        # Set the default colormap
-        self.colormap = "bone"
 
         # Initialise canvas
         self.static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
@@ -96,6 +98,8 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         navbar._actions['save_figure'].disconnect()
         navbar._actions['save_figure'].triggered.connect(self.save_display)
 
+        navbar._actions['home'].triggered.connect(lambda: print("testing"))
+
         self.img = None
 
         # Dynamically generate menu full of all available colourmaps. Do not add the inverted ones.
@@ -106,7 +110,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             if colour[-2:] != "_r":
                 cm = self.colMenu.addAction(colour)
                 cm.setCheckable(True)
-                if colour==self.colormap:
+                if colour==self.settings["heatmap"]["cmap"][:-2]:
                     cm.setChecked(True)
                 receiver = lambda checked, cmap=colour: self.img.set_cmap(cmap)
                 cm.triggered.connect(receiver)
@@ -136,9 +140,6 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         # Keep track of the current view window
         self.last_zoomed = [self._hm_xrange[:], self._hm_yrange[:]]
 
-        # Set the default algorithm
-        self.algo = "GCC"
-
         # Set the default EPSG codes
         self._EPSG = 4326         # WGS84 (lat/long)   https://epsg.io/4326
         self._projected_EPSG=2193 # NZTM2000 projected https://epsg.io/2193
@@ -151,10 +152,8 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         # Keep track of the currently opened file
         self.open_filename = ""
-
-        # Initialise the lakeator object
-        self.get_mic_locs()
-        self.loc = lakeator.Lakeator(self._micLocs)
+        
+        self.loc = lakeator.Lakeator(self.settings["array"]["mic_locations"])
 
     def ondraw(self, event):
         """Return the new axis limits when the screen is resized"""
@@ -163,22 +162,21 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
     def invert_heatmap(self):
         """Adds or removes _r to the current colourmap before setting it and redrawing the canvas"""
-        print(self.img.get_cmap())
-        if self.colormap[-2:] == "_r":
-            self.colormap = self.colormap[:-2]
-            self.img.set_cmap(self.colormap)
+        if self.settings["heatmap"]["cmap"][-2:] == "_r":
+            self.settings["heatmap"]["cmap"] = self.settings["heatmap"]["cmap"][:-2]
+            self.img.set_cmap(self.settings["heatmap"]["cmap"])
             self.static_canvas.draw()
         else:
             try:
-                self.img.set_cmap(self.colormap + "_r")
-                self.colormap = self.colormap + "_r"
+                self.img.set_cmap(self.settings["heatmap"]["cmap"] + "_r")
+                self.settings["heatmap"]["cmap"] = self.settings["heatmap"]["cmap"] + "_r"
                 self.static_canvas.draw()
             except ValueError as inst:
                 print(type(inst), inst)
 
     def _setcol(self, c):
         """Sets the colourmap attribut to the name of the cmap - needed as I'm using strings to set the cmaps rather than cmap objects"""
-        self.colormap = self.img.get_cmap().name
+        self.settings["heatmap"]["cmap"] = self.img.get_cmap().name
 
     def generate_heatmap(self):
         """Calculate and draw the heatmap"""
@@ -190,15 +188,14 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         # Show a loading message while the user waits
         self.statusBar().showMessage('Calculating heatmap...')
-        dom = self.loc.estimate_DOA_heatmap(self.algo, xrange=self.last_zoomed[0], yrange=self.last_zoomed[1], no_fig=True)
+        dom = self.loc.estimate_DOA_heatmap(self.settings["algorithm"]["current"], xrange=self.last_zoomed[0], yrange=self.last_zoomed[1], no_fig=True)
 
         # Show the image and set axis labels & title      
-        self.img = self._static_ax.imshow(dom, cmap=self.colormap, interpolation='none', origin='lower',
+        self.img = self._static_ax.imshow(dom, cmap=self.settings["heatmap"]["cmap"], interpolation='none', origin='lower',
                    extent=[self.last_zoomed[0][0], self.last_zoomed[0][1], self.last_zoomed[1][0], self.last_zoomed[1][1]])
-        print(type(self.img))
         self._static_ax.set_xlabel("Horiz. Dist. from Center of Array [m]")
         self._static_ax.set_ylabel("Vert. Dist. from Center of Array [m]")
-        self._static_ax.set_title("{}-based Source Location Estimate".format(self.algo))
+        self._static_ax.set_title("{}-based Source Location Estimate".format(self.settings["algorithm"]["current"]))
 
         # Add a colourbar and redraw the screen
         self.static_canvas.figure.colorbar(self.img)
@@ -214,16 +211,6 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self._has_heatmap = True
         return
 
-    def get_mic_locs(self, filePath="arrayconfig.txt"):
-        """Load the microphone locations from disk"""
-        lt = []
-        with open(filePath, 'r') as f:
-            ltemp = f.readlines()
-        for l in ltemp:
-            lt.append([float(x) for x in l.strip().split(',')])
-        self._micLocs = np.array(lt)
-        return
-
     def file_open(self):
         """Let the user pick a file to open, and then calculate the cross-correlations"""
         self.statusBar().showMessage('Loading...')
@@ -236,7 +223,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
     
     def save_display(self):
         """Save the heatmap and colourbar with a sensible default filename"""
-        defaultname = self.open_filename[:-4] + "_" + self.algo + "_heatmap.png"
+        defaultname = self.open_filename[:-4] + "_" + self.settings["algorithm"]["current"] + "_heatmap.png"
         name, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save image", defaultname, "PNG files *.png;; All Files *")
         name = name + ".png"
         self.static_canvas.figure.savefig(name)
@@ -244,28 +231,60 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
     def get_GPS_info(self):
         """Listener for the change GPS menu item"""
-        self.setGPSInfoDialog = Dialogs.GPSPopUp(coords=self._GPS_coords, EPSG=self._EPSG)
+        self.setGPSInfoDialog = Dialogs.GPSPopUp(coords=self.settings["array"]["GPS"]["coordinates"], 
+                                                EPSG=self.settings["array"]["GPS"]["EPSG"]["input"], 
+                                                pEPSG=self.settings["array"]["GPS"]["EPSG"]["projected"], 
+                                                tEPSG=self.settings["array"]["GPS"]["EPSG"]["target"],)
         self.setGPSInfoDialog.activate.clicked.connect(self.changeGPSInfo)
         self.setGPSInfoDialog.exec()
 
     def changeGPSInfo(self):
-        """ Listener for the operator/reviewer dialog.
+        """ Listener for the change gps info dialog.
         """
-        lat, long, EPSG, projEPSG, targetEPSG = self.setGPSInfoDialog.getValues()
-        self._GPS_coords = (lat, long)
-        self._EPSG = EPSG
+        lat, long, EPSG, projEPSG, targetEPSG = self.setGPSInfoDialog.getValues() 
+        self.settings["array"]["GPS"]["EPSG"]["input"] = EPSG
+        self.settings["array"]["GPS"]["EPSG"]["projected"] = projEPSG
+        self.settings["array"]["GPS"]["EPSG"]["target"] = targetEPSG
+        self.settings["array"]["GPS"]["coordinates"] = (lat, long)
+
         self._has_GPS = True
         if self._has_heatmap:
             self.saveGisAction.setDisabled(False)
-        # self.statusRight.setText("Operator: " + self.operator + ", Reviewer: "+self.reviewer)
         self.setGPSInfoDialog.close()
 
+    def get_array_info(self):
+        """Listener for the change array info menu item"""
+        self.setMicsInfoDialog = Dialogs.MicPositionPopUp(cur_locs=self.settings["array"]["mic_locations"])
+        self.setMicsInfoDialog.activate.clicked.connect(self.changeArrayInfo)
+        self.setMicsInfoDialog.exec()
+
+    def changeArrayInfo(self):
+        """ Listener change array info dialog.
+        """
+        miclocs = self.setMicsInfoDialog.getValues()
+        self.settings["array"]["mic_locations"] = miclocs
+        self.loc = lakeator.Lakeator(self.settings["array"]["mic_locations"])
+        self.setMicsInfoDialog.close()
+
     def exportGIS(self):
-        defaultname = self.open_filename[:-4] + "_" + self.algo + "_heatmap.tif"
+        defaultname = self.open_filename[:-4] + "_" + self.settings["algorithm"]["current"] + "_heatmap.tif"
         name, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save image & GIS Metadata", defaultname, "TIF files *.tif;; All Files *")
         name = name + ".tif"
-        self.loc.heatmap_to_GIS(self._GPS_coords, self._EPSG, projected_EPSG=self._projected_EPSG, target_EPSG=self._target_EPSG, filepath=name)
+        self.loc.heatmap_to_GIS(self.settings["array"]["GPS"]["coordinates"], 
+                                self.settings["array"]["GPS"]["EPSG"]["input"], 
+                                projected_EPSG=self.settings["array"]["GPS"]["EPSG"]["projected"], 
+                                target_EPSG=self.settings["array"]["GPS"]["EPSG"]["target"], 
+                                filepath=name)
         return
+
+    def _load_settings(self, settings_file="./settings.txt"):
+        with open(settings_file, "r") as f:
+            self.settings = json.load(f)
+    
+    def _save_settings(self, settings_file="./settings.txt"):
+        with open(settings_file, "w") as f:
+            self.settings = json.dumps(f, sort_keys=True, indent=4)
+
 
 
 # Run the thing
